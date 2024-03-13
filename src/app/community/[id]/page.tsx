40845da0f672715cde80fbe7d80068217c48c3b7
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-03-13 10:49:12 +0900

    🎉Init: Next, TS, TailwindCSS 셋팅

---
+++ src/app/community/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CommunityDetailPage = () => {
+  return <div className="w-full">CommunityDetail 페이지입니당</div>;
+};
+
+export default CommunityDetailPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainPage = () => {
+  return <div className="w-full">메인 페이지입니당</div>;
+};
+
+export default MainPage;
