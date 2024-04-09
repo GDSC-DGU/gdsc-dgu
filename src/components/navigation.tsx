72cb912ce20c8240090666183400599204eef39a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-04-10 03:37:55 +0900

    ✨Feat: Timeline추가

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -18,6 +18,7 @@ export default function Navigation() {
       <div className="w-4/5 px-10 justify-between inline-flex text-xl font-bold">
         <Link href="/">GDSC DGU</Link>
         <div className="justify-start items-start gap-10 flex">
+          <Link href="/timeline">Timeline</Link>
           <Link href="/seminar">Seminar</Link>
           <Link href="/project">Project</Link>
           <Link href="/member">Member</Link>
