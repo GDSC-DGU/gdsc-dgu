93cf01453bcfb6449ff1f493123b06c8291df32e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-04-12 01:49:36 +0900

    ✨Feat: SVG 셋팅 #12

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    );
+    // https://github.com/langchain-ai/langchainjs/issues/943#issuecomment-1544928533
+    config.externals = [...config.externals, 'hnswlib-node'];
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
+};
 
 export default nextConfig;
