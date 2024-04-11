813538748d77362f814ea8f847db64fd295be29a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-04-12 01:50:38 +0900

    ✨Feat: 모집 배너 제작 #12

---
+++ src/components/recruit/RecruitHeader.tsx
@@ -0,0 +1,44 @@
+import BannerImg from '@/images/recruit_banner.png';
+import BannerImg2 from '@/svg/recruit_banner2.svg';
+import ChervonDownIcon from '@/svg/icons/common/chervon_down.svg';
+
+/**
+ * @description
+ * 모집 페이지 헤더 컴포넌트
+ * @component RecruitHeader
+ * @returns {JSX.Element} RecruitHeader
+ * @since 2024.04.12
+ */
+/**
+ * Renders the header component for the recruitment section.
+ * @returns The rendered header component.
+ */
+const RecruitHeader = () => {
+  return (
+    <header className="h-lvh flex flex-col">
+      {/* Main section */}
+      <main
+        className="w-full flex-col justify-center items-center gap-2.5 inline-flex h-[35.75rem] 2xl:h-[40rem] min-h-fit mt-10"
+        style={{
+          backgroundImage: `url(${BannerImg.src})`,
+          backgroundSize: 'cover', // Sets the background image to cover the div
+          backgroundPosition: 'center', // Aligns the background image to the center
+        }}
+      >
+        <article className="flex flex-col items-center">
+          <BannerImg2 className="scale-75" />
+          <div className="flex-col items-center flex mt-14">
+            <p className="text-center text-white text-xs font-normal leading-[14px] tracking-wide">
+              2023/08/28 ~ 2023/09/14
+            </p>
+            <p className="border mt-8 py-2 px-28 border-solid text-zinc-500 text-sm font-medium">
+              모집 기간이 아닙니다.
+            </p>
+          </div>
+        </article>
+      </main>
+      <ChervonDownIcon className="self-center " />
+    </header>
+  );
+};
+export default RecruitHeader;
