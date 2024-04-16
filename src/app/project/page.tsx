2a7944c2190c0d1e19bc2da6a139fd0191fb9d5a
nakyong82 <nakyong7770@gmail.com>
2024-04-16 22:53:36 +0900

    ✨Feat: 상단 제목 제작

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,3 +1,5 @@
+import ProjectHeader from '@/components/project/header/ProjectHeader';
+import ProjectList from '@/components/project/list/ProjectList';
 import React from 'react';
 
 export const metadata = {
@@ -5,7 +7,18 @@ export const metadata = {
 };
 
 const ProjectPage = () => {
-  return <div className="w-4/5 px-10">Project 페이지입니당</div>;
+  return (
+    <main className="w-full px-10">
+      {/* -----------------------------------------------*/}
+      {/* -------------------- 제목 ----------------------*/}
+      {/* -----------------------------------------------*/}
+      <ProjectHeader />
+      {/* -----------------------------------------------*/}
+      {/* ------------------ 프로젝트 리스트 ----------------*/}
+      {/* -----------------------------------------------*/}
+      <ProjectList />
+    </main>
+  );
 };
 
 export default ProjectPage;
---
+++ src/components/project/header/ProjectHeader.tsx
@@ -0,0 +1,23 @@
+/**
+ * @description
+ * 프로젝트 페이지 헤더 컴포넌트
+ * @component ProjectHeader
+ * @returns {JSX.Element} ProjectHeader
+ * @since 2024.04.16
+ */
+/**
+ * Renders the header component for the project section.
+ * @returns The rendered header component.
+ */
+
+const ProjectHeader = () => {
+  return (
+    <section className="w-4/5 flex flex-col mx-auto my-10">
+      <div className="px-3 pt-10 text-5xl leading-[3.5rem]">Project</div>
+      <div className="px-3 pt-3 leading-[1.75rem]">GDSC DGU의 프로젝트</div>
+      <div className="w-full pt-6 border-b border-white border-solid"></div>
+    </section>
+  );
+};
+
+export default ProjectHeader;
