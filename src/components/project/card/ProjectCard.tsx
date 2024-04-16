5b79df23e2b58816c005f4d21cacdd49c5d6d0cb
nakyong82 <nakyong7770@gmail.com>
2024-04-16 22:53:49 +0900

    ✨Feat: 프로젝트 리스트 제작

---
+++ src/components/project/card/ProjectCard.tsx
@@ -0,0 +1,39 @@
+import ProjectImg from '@/images/project_img.png';
+
+/**
+ * @description
+ * 프로젝트 페이지 프로젝트 카드 컴포넌트
+ * @component ProjectCard
+ * @returns {JSX.Element} ProjectCard
+ * @since 2024.04.16
+ */
+/**
+ * Renders the card component for the project section.
+ * @returns The rendered card component.
+ */
+
+const ProjectCard = () => {
+  return (
+    <section>
+      <div
+        className="w-full h-[12.375rem] rounded-md flex-col justify-center items-center"
+        style={{
+          backgroundImage: `url(${ProjectImg.src})`,
+          backgroundSize: 'cover', // Sets the background image to cover the div
+          backgroundPosition: 'center', // Aligns the background image to the center
+        }}
+      ></div>
+      <div className="w-full p-4 pt-3 rounded-b-md bg-[#1C1D1F]">
+        <div className="pb-4 text-[1.25rem] leading-7">제목</div>
+        <div className="pb-5 text-sm leading-6">
+          서비스 소개! 두줄까지 적어주세요.
+        </div>
+        <div className="text-xs leading-[0.875rem] text-[#9FA4A8]">
+          특이 사항
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectCard;
---
+++ src/components/project/list/ProjectList.tsx
@@ -0,0 +1,32 @@
+import ProjectCard from '../card/ProjectCard';
+
+/**
+ * @description
+ * 프로젝트 페이지 프로젝트 리스트 컴포넌트
+ * @component ProjectList
+ * @returns {JSX.Element} ProjectList
+ * @since 2024.04.16
+ */
+/**
+ * Renders the list component for the project section.
+ * @returns The rendered list component.
+ */
+
+const ProjectList = () => {
+  return (
+    <section>
+      <section className="w-4/5 grid xl:grid-cols-3 md:grid-cols-2 gap-8 mx-auto">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </section>
+      <section>페이지</section>
+    </section>
+  );
+};
+
+export default ProjectList;
