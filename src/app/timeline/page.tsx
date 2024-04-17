7080c4d797dccea4cbaf529f0763be0b8deb28ac
puretension <rlrlfhtm5@gmail.com>
2024-04-18 02:55:15 +0900

    ✨Feat: 각 기수별 필터링 버튼 생성후 적용  #15

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+// src/app/timeline/page.tsx
+'use client';
+import React, { useState } from 'react';
+import TimelineHeader from "@/components/timeline/header/TimelineHeader";
+import GenerationButton from "@/components/timeline/button/GenerationButton";
+import TimelineMonthlyPart from "@/components/timeline/part/TimelineMonthlyPart";
+import TIMELINE_CARDS_INFO from "@/constants/timeline/timelineCardInfo";
 
-const TimelinePage = () => {
-  return <div className="w-4/5 px-10">Timeline 페이지입니당</div>;
+export default function TimelinePage() {
+  const [activeGeneration, setActiveGeneration] = useState<string>('1st');
+
+  const handleButtonClick = (generation: string): void => {
+    setActiveGeneration(generation);
+  };
+
+  return (
+    <main className="w-full px-10">
+      <TimelineHeader />
+      <section className="w-4/5 flex flex-col mx-auto my-10">
+        <section className="w-full flex justify-start items-center gap-3">
+          {['1st', '2nd', '3rd'].map(generation => (
+            <GenerationButton
+              key={generation}
+              label={generation}
+              isActive={activeGeneration === generation}
+              onClick={() => handleButtonClick(generation)}
+            />
+          ))}
+        </section>
+      </section>
+      <TimelineMonthlyPart timelineData={TIMELINE_CARDS_INFO[activeGeneration]} />
+    </main>
+  );
 };
-
-export default TimelinePage;
