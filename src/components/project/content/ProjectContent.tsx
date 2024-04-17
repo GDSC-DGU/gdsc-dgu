30b0a17ca0661d1be0e9f855f5116b2f316ed9aa
nakyong82 <nakyong7770@gmail.com>
2024-04-18 00:34:26 +0900

    ✨Feat: 프로젝트 상세 페이지 내용 제작

--- src/components/project/content/ProjectContent.tsx
+++ src/components/project/content/ProjectContent.tsx
@@ -1,3 +1,6 @@
+import ProjectMember from './ProjectMember';
+import ToggleIcon from '@/svg/icons/project/toggleIcon.svg';
+
 /**
  * @description
  * 프로젝트 상세 페이지 프로젝트 내용 컴포넌트
@@ -11,7 +14,28 @@
  */
 
 const ProjectContent = () => {
-  return <section>내용</section>;
+  return (
+    <section className="w-4/5 px-10 pb-[7.5rem] mx-auto">
+      <section>
+        <div className="w-full flex flex-row justify-between items-center mb-8 px-2 pb-3 border-b border-solid border-[#3E4348]">
+          <div className="text-[2.125rem] leading-[2.5rem]">Team</div>
+          <button className="">
+            <ToggleIcon />
+          </button>
+        </div>
+        <div className="w-full grid min-[960px]:grid-cols-2 gap-8">
+          <ProjectMember />
+          <ProjectMember />
+          <ProjectMember />
+          <ProjectMember />
+        </div>
+      </section>
+      <section className="mt-[5rem] leading-7">
+        노션 내용 크롤링 <br /> CONG(콩) : 축하 아카이빙 서비스 <br /> CONG은 왜
+        필요할까? <br />{' '}
+      </section>
+    </section>
+  );
 };
 
 export default ProjectContent;
