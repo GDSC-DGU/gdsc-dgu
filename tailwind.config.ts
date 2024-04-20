2dba07095389d6eb8751cfbacd0c1732fa31c551
shimseohyun <shim020924@gmail.com>
2024-04-20 15:12:50 +0900

    💄 Style - aboutGDSC image map

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       screens: {
-        desktop: '769px',
+        desktop: '961px',
         tablet: '521px',
       },
       colors: {
@@ -35,12 +34,12 @@ const config: Config = {
       animation: {
         fadeIn: 'fadeIn 1s forwards',
       },
-        spacing: {
-          '4.75': '19px' // 19px를 나타내는 키를 추가합니다.
-        },
-          width: {
-            '70rem': '70rem', // '70rem'이라는 key로 70rem 너비를 설정
-          },
+      spacing: {
+        '4.75': '19px', // 19px를 나타내는 키를 추가합니다.
+      },
+      width: {
+        '70rem': '70rem', // '70rem'이라는 key로 70rem 너비를 설정
+      },
     },
   },
   plugins: [],
