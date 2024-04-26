4442f0397c74910661bf0c495474cde733058c37
puretension <rlrlfhtm5@gmail.com>
2024-04-26 19:18:17 +0900

    ♻️ Refactor: 불필요 코드 제거 #30

--- src/components/timeline/button/GenerationButton.tsx
+++ src/components/timeline/button/GenerationButton.tsx
@@ -1,16 +1,14 @@
+import React from 'react';
+
 /* 
 기수별 버튼 컴포넌트(1st, 2nd, 3rd)
 */
-import React from 'react';
 
 const GenerationButton: React.FC<GenerationButtonProps> = ({ label, isActive, onClick }) => {
-  const borderColor = isActive ? 'white' : '#536E7B';
-  const textColor = isActive ? 'white' : '#536E7B';
-
   return (
     <button
-      className="w-10 h-8 bg-transparent border rounded-lg text-xs leading-4 px-2 py-1"
-      style={{ borderWidth: '1.5px', borderColor, color: textColor }}
+      className="border rounded-lg B2 px-3 py-0.5"
+      style={{ borderWidth: '1px', borderColor: isActive ? 'white' : '#536E7B', color: isActive ? 'white' : '#536E7B' }}
       onClick={() => onClick(label)}
     >
       {label}
