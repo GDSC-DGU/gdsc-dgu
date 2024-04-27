bf97eaf4344fcc47db8a6f07ec90b9e46657fadd
nakyong82 <nakyong7770@gmail.com>
2024-04-27 11:59:09 +0900

    🎨 Style: 프로젝트 카드 내용 height 고정 및 2줄 이상 말줄임표 #31

--- src/components/project/card/ProjectCard.tsx
+++ src/components/project/card/ProjectCard.tsx
@@ -26,9 +26,13 @@ const ProjectCard = () => {
           }}
         ></div>
         <div className="w-full p-4 pt-3 rounded-b-md bg-mono_900">
-          <div className="pb-4 H6">제목</div>
-          <div className="pb-5 B2">서비스 소개! 두줄까지 적어주세요.</div>
-          <div className="Cap text-mono_400">특이 사항</div>
+          <div className="pb-1 H6">제목</div>
+          <div className="B2 h-[3.2rem] text-overflow-custom">
+            서비스 소개! 두줄까지 적어주세요. 서비스 소개! 두줄까지 적어주세요.
+            서비스 소개! 두줄까지 적어주세요. 서비스 소개! 두줄까지 적어주세요.
+            서비스 소개! 두줄까지 적어주세요.
+          </div>
+          <div className="pt-5 Cap text-mono_400">특이 사항</div>
         </div>
       </Link>
     </section>
