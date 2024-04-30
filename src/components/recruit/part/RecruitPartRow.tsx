d87de5d0a9f3ab9fd1b9a66044b272d1cfb976be
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-05-01 03:42:31 +0900

    💄Design : Recruit Target PartRow #35

--- src/components/recruit/part/RecruitPartRow.tsx
+++ src/components/recruit/part/RecruitPartRow.tsx
@@ -8,7 +8,7 @@ import { RecruitPart } from '@/interfaces/recruit/recruitPart';
 const RecruitPartRow = ({ recruitPart }: { recruitPart: RecruitPart }) => {
   return (
     <section className="self-stretch justify-start items-start gap-8 flex">
-      <div className=" w-96 h-60 p-8 bg-black rounded flex-col justify-start items-start gap-5 relative  overflow-hidden">
+      <div className=" w-96 h-60 p-8 bg-black rounded flex-col justify-start items-start gap-5 relative overflow-hidden">
         {/* 배경으로 사용할 div */}
         <div className="absolute top-0 left-0 w-full h-full z-0">
           <div
@@ -33,22 +33,16 @@ const RecruitPartRow = ({ recruitPart }: { recruitPart: RecruitPart }) => {
             )}
           </div>
           <div className="flex-col justify-start items-start gap-3 flex">
-            <div className="text-xl font-medium leading-7 tracking-tight">
-              {recruitPart.title}
-            </div>
-            <div className="text-sm leading-normal">
-              {recruitPart.description}
-            </div>
+            <div className="H6">{recruitPart.title}</div>
+            <div className="B2">{recruitPart.description}</div>
           </div>
         </div>
       </div>
 
       <div className="grow p-8 bg-zinc-900 rounded flex-col justify-start items-start gap-6 flex">
         <div className="self-stretch flex-col justify-start items-start gap-2 flex">
-          <div className=" text-xl font-medium  leading-7 tracking-tight">
-            Preferred
-          </div>
-          <div className="leading-7">
+          <div className="H6">Preferred</div>
+          <div className="B1">
             {recruitPart.preferred[0]}
             <br />
             {recruitPart.preferred[1]}
