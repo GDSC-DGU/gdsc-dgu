2a8ff6aa9ebc26f441269a05f3c8c0d65d5b545e
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-05-04 01:42:29 +0900

    ✨Feat: 사전알림신청 구글폼 연동

--- src/components/recruit/header/RecruitHeader.tsx
+++ src/components/recruit/header/RecruitHeader.tsx
@@ -1,6 +1,7 @@
 import BannerImg from '@/images/recruit_banner.png';
 import BannerImg2 from '@/svg/recruit_banner2.svg';
 import ChervonDownIcon from '@/svg/icons/common/chervon_down.svg';
+import Link from 'next/link';
 
 /**
  * @description
@@ -17,6 +18,7 @@ const RecruitHeader = () => {
   return (
     <header className="h-lvh flex flex-col">
       {/* Main section */}
+
       <div
         className="w-full flex-col justify-center items-center gap-2.5 inline-flex h-[35.75rem] 2xl:h-[40rem] min-h-fit mt-10"
         style={{
@@ -29,11 +31,14 @@ const RecruitHeader = () => {
           <BannerImg2 className="scale-75 max-md:scale-50" />
           <div className="flex-col items-center flex mt-14">
             <p className="text-center Cap tracking-wide">
-              2023/08/28 ~ 2023/09/14
+              2기 모집 기간 <br />
+              2024/08/28 ~ 2024/09/14
             </p>
-            <p className="border mt-8 py-4 px-28 border-solid text-mono_500 Btn">
-              모집 기간이 아닙니다.
-            </p>
+            <Link href="https://forms.gle/MGWVqDKK5zwUTjes5" target="_blank">
+              <p className="border mt-8 py-4 px-28 border-solid text-mono_500 Btn">
+                2기 모집 사전 알림 신청 하기
+              </p>
+            </Link>
           </div>
         </article>
       </div>
