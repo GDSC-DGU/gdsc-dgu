3fe5629f5a19395f2d815238684ecb211e408e23
bunju20 <ghkd2271@naver.com>
2024-05-08 22:44:31 +0900

    🔧 Chore: 수정해주신거 원래대로,swiper/css/autoplay 임포트 추가

--- src/constants/member/timeLine.ts
+++ src/constants/member/timeLine.ts
@@ -8,11 +8,7 @@ export const TIME_LINE = [
     date: '2023.9',
   },
   {
-    generate: '2st',
+    generate: '2nd',
     date: '2024.9',
-  },
-  {
-    generate: '3st',
-    date: '2025.9',
   },
 ];
