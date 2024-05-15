f3012c3805821815c396893187ba71ba8b5f737d
Eunji <129590633+bianbbc87@users.noreply.github.com>
2024-05-16 06:45:15 +0900

    ♻️Refactor - 세미나 발표자 default 이미지로 이름 변경 #44

--- src/constants/seminar/seminarMemberData.ts
+++ src/constants/seminar/seminarMemberData.ts
@@ -1,5 +1,5 @@
 import { SeminarMember } from "@/interfaces/seminar/seminarMember";
-import PresenterProfileImg from '@/images/seminar/presenter/presenter_profile_img.png';
+import PresenterProfileImg from '@/images/seminar/presenter/default_presenter.png';
 
 export const SEMINAR_MEMBER_DATA: SeminarMember = {
     id: 1,
